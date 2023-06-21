679ebb6e086b795fd73ee21216eb1ddb729ca1cb
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-06-20 21:17:07 -0400

    tweeter app

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+
+$(document).ready(() => {
+  $("#tweet-text").on("input", function(e) {
+    $("#empty-error").slideUp();
+    if (e.target.value.length < 141) {
+      $("#too-long-error").slideUp();
+      $("div.tweet-button-counter").find(".counter")
+        .html(140 - e.target.value.length)
+        .css("color", "#545149");
+    }
+    if (e.target.value.length > 140) {
+      $("div.tweet-button-counter").find(".counter")
+        .html(140 - e.target.value.length)
+        .css("color", "red");
+    }
+  });
+});
